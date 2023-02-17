f48fc062e60c0085503cbbc6f9eeacbb41ac34f5
RickSnRz <rickx213@gmail.com>
2023-02-17 12:43:14 -0500

    Practica commit

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
+
+//var parf = document.getElementById("parafo").textContent
+//alert(parf)
+
+var nuevotexto = document.createTextNode("Texto hijo")
+var miDiv = document.getElementById("div1")
+miDiv.appendChild(nuevotexto)
+
+
 var dias = ["Lunes", "Martes", "Miercoles", "Juevez", "Viernes",
 "Sabado", "Domingo"]
 
-//for(var i=0;i<dias.length;i++){alert(dias[i]);}
+//for(var i=0;i<dias.length;i++){alert(dias[i])}
+
